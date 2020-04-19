24e82f7ea124f0cd80d061fe0e9b3b3de522cffc
hxj <871378581@qq.com>
2020-04-19 22:24:21 +0800

    数据匹配

--- adminservice/src/main/java/com/segama/ege/service/ConfigService.java
+++ adminservice/src/main/java/com/segama/ege/service/ConfigService.java
@@ -4,13 +4,14 @@ import com.segama.ege.entity.AdminSystemConfig;
 import com.segama.ege.entity.AdminSystemConfigExample;
 import com.segama.ege.repository.AdminSystemConfigMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+@Service
 public class ConfigService {
 
     @Autowired
